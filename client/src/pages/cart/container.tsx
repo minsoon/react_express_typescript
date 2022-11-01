4b5b0fb507b95f50663daea92b1638f83cd5753a
minchsung <minchsung@gmarket.com>
2022-11-01 11:48:07 +0900

    불필요한 코드 삭제

--- client/src/pages/cart/container.tsx
+++ client/src/pages/cart/container.tsx
@@ -1,22 +1,6 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import React from 'react'
 
 const Cart = () => {
-    const [name, setName] = useState('')
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const response = await axios.get(
-                `${process.env.REACT_APP_API_URL}/api/get/1`
-                , {
-                    params: {
-                        name: 'mcsung'
-                    }
-                });
-            setName(response.data.name);
-        }
-        return () => {
-            fetchUsers();
-        }
-    }, []);
 }
+
 export default Cart;
